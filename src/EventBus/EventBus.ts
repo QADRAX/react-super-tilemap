51d58b60ef31902ad744cbc9dce3369cdf7c1aaf
QADRAX <carlos.delacuadra@devo.com>
2023-02-17 22:16:02 +0100

    chore: event bus types

--- src/EventBus/EventBus.ts
+++ src/EventBus/EventBus.ts
@@ -1,17 +1,5 @@
-export type EventKey = string | symbol
-export type EventHandler<T = any> = (payload: T) => void
-export type EventMap = Record<EventKey, EventHandler>
-export type Bus<E> = Record<keyof E, E[keyof E][]>
+import { Bus, EventBusChannel, EventBusConfig, EventMap } from "./EventBus.types"
 
-export interface EventBusChannel<T extends EventMap> {
-  on<Key extends keyof T>(key: Key, handler: T[Key]): () => void
-  off<Key extends keyof T>(key: Key, handler: T[Key]): void
-  emit<Key extends keyof T>(key: Key, ...payload: Parameters<T[Key]>): void
-}
-
-export interface EventBusConfig {
-  onError: (...params: any[]) => void
-}
 
 export function createEventBusChannel<E extends EventMap>(
   config?: EventBusConfig
